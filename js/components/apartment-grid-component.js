04b05945d4691d37173430c7833816f692e3b851
Justas Peldžius <peldjustas@gmail.com>
2021-11-10 09:18:33 +0200

    6: used API method to get data in ApartmentGridComponent

--- js/components/apartment-grid-component.js
+++ js/components/apartment-grid-component.js
@@ -1,6 +1,34 @@
 class ApartmentGridComponent {
 	constructor() {
-		this.htmlElement = document.createElement('div');
-		this.htmlElement.innerHTML = 'ApartmentGridComponent';
+		this.state = {
+			apartaments: [],
+		};
+
+		this.init();
 	}
+
+	showError = (error) => console.error(error);
+
+	saveApartaments = (apartaments) => {
+		this.state = { apartaments };
+		this.render();
+	};
+
+	getApartaments = () =>
+		API.fetchApartments(this.saveApartaments, this.showError);
+
+	init = () => {
+		this.getApartaments();
+		this.htmlElement = document.createElement('div');
+		this.render();
+	};
+
+	render = () => {
+		const { apartaments } = this.state;
+		if (apartaments.length === 0) {
+			this.htmlElement.innerHTML = `<h1>Siunčiama...</h1>`;
+		} else if (apartaments.length > 0) {
+			this.htmlElement.innerHTML = `<h1>Parsiųsta!</h1>`;
+		}
+	};
 }
